12503a75411fe6857daa9b3552e3e1eb25eb632e
BerylSystems <vinay@berylsystems.com>
2015-03-27 19:02:58 +0530

    Default Tag is being used for localhost:3000 and if nothing tag is given as input

--- instagram/app/controllers/home_controller.rb
+++ instagram/app/controllers/home_controller.rb
@@ -1,12 +1,14 @@
 class HomeController < ApplicationController
   def index
-  	 @instagram = Instagram.user_recent_media("1792475136")
+  	@instagram = Instagram.user_recent_media("1792475136")
 
-     client = Instagram.client(:access_token => session[:access_token])
-     #html = "<h1>Search for tags, get tag info and get media by tag</h1>"
-     
-     tags = client.tag_search(params[:q])
-     @tag = client.tag_recent_media(tags[0].name)
-     @count=  tags[0].media_count
+    client = Instagram.client(:access_token => session[:access_token])
+    unless params[:q].blank?
+      tags = client.tag_search(params[:q])
+      @tag = client.tag_recent_media(tags[0].name)
+      @count=  tags[0].media_count
+    else
+  	  @tag = client.tag_recent_media('persistent')
+    end
   end
 end
