c2f1951c7b753747e9380e9539ed924bd6741f9c
BerylSystems <vinay@berylsystems.com>
2015-03-27 16:28:02 +0530

    Tag Search of media Text field serches the input tag media Dont know but there is limitation of media shown also tells about the toatal count of pics of a input tag

--- instagram/app/controllers/home_controller.rb
+++ instagram/app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
   	 @instagram = Instagram.user_recent_media("1792475136")
+
+     client = Instagram.client(:access_token => session[:access_token])
+     #html = "<h1>Search for tags, get tag info and get media by tag</h1>"
+     
+     tags = client.tag_search(params[:q])
+     @tag = client.tag_recent_media(tags[0].name)
+     @count=  tags[0].media_count
   end
 end
