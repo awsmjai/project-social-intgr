a4659e1426f421e7ce9227767e13403eb78fea1c
BerylSystems <vinay@berylsystems.com>
2015-04-01 19:17:02 +0530

    Pagination Used for tag media -to show media of numbers of pages TODO:Synchronisation between location and tags

--- instagram/app/controllers/home_controller.rb
+++ instagram/app/controllers/home_controller.rb
@@ -3,13 +3,24 @@ class HomeController < ApplicationController
   	@instagram = Instagram.user_recent_media("1792475136")
 
     client = Instagram.client(:access_token => session[:access_token])
-    #@media=client.media_search("37.7808851","-122.3948632")
+    @media=client.media_search("28.6139","77.2090")
     unless params[:q].blank?
       tags = client.tag_search(params[:q])
-      @tag = client.tag_recent_media(tags[0].name, options = {:count => 100})
+      @tag1 = client.tag_recent_media(tags[0].name, count: 100)
+      @max_tag_id1 = @tag1.pagination.next_max_tag_id
+      @tag2 = client.tag_recent_media(tags[0].name,count: 100, max_id: @max_tag_id1) unless @max_tag_id1.nil?
+      @max_tag_id2 = @tag2.pagination.next_max_tag_id
+      @tag3 = client.tag_recent_media(tags[0].name,count: 100, max_id: @max_tag_id2) unless @max_tag_id2.nil?
+      @max_tag_id3 = @tag3.pagination.next_max_tag_id
+      @tag4 = client.tag_recent_media(tags[0].name,count: 100, max_id: @max_tag_id3) unless @max_tag_id3.nil?
+      @max_tag_id4 = @tag4.pagination.next_max_tag_id
+  
       @count=  tags[0].media_count
     else
-  	  @tag = client.tag_recent_media('persistent')
+  	  @tag1 = client.tag_recent_media('worldcup2015')
+      @tag2 = client.tag_recent_media('teamindia')
+      @tag3 = client.tag_recent_media('nzvsaus')
+      @tag4 = client.tag_recent_media('indiavsaus')
     end
      @location = Location.all
   end
