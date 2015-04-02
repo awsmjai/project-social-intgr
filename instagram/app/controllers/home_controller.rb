1f2d9a3699190bca6f13ebbfc02dcdf7deb0338a
BerylSystems <vinay@berylsystems.com>
2015-04-02 19:01:10 +0530

    Changes in Location search -Now Location input label is shown on main home with tag search -As before for location serch need to go different page TODO:Sync between tag and location

--- instagram/app/controllers/home_controller.rb
+++ instagram/app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @location = Location.new
   	@instagram = Instagram.user_recent_media("1792475136")
-
     client = Instagram.client(:access_token => session[:access_token])
-    @media=client.media_search("28.6139","77.2090")
     unless params[:q].blank?
       tags = client.tag_search(params[:q])
       @tag1 = client.tag_recent_media(tags[0].name, count: 100)
@@ -14,7 +13,6 @@ class HomeController < ApplicationController
       @max_tag_id3 = @tag3.pagination.next_max_tag_id
       @tag4 = client.tag_recent_media(tags[0].name,count: 100, max_id: @max_tag_id3) unless @max_tag_id3.nil?
       @max_tag_id4 = @tag4.pagination.next_max_tag_id
-  
       @count=  tags[0].media_count
     else
   	  @tag1 = client.tag_recent_media('worldcup2015')
@@ -22,6 +20,5 @@ class HomeController < ApplicationController
       @tag3 = client.tag_recent_media('nzvsaus')
       @tag4 = client.tag_recent_media('indiavsaus')
     end
-     @location = Location.all
   end
 end
