2c9923addaded2fa62aa3b283d9ed584ad88ffcb
BerylSystems <vinay@berylsystems.com>
2015-04-03 19:01:52 +0530

    some changes

--- twitter/config/routes.rb
+++ twitter/config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'home#index'
+  match 'auth/:provider/callback', to: 'sessions#create', via: :get
+  match 'auth/failure', to: redirect('/'), via: :get
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: :get
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  get '/search' => 'home#index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
--- twitter/db/schema.rb
+++ twitter/db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150403072433) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider",     limit: 255
+    t.string   "uid",          limit: 255
+    t.string   "name",         limit: 255
+    t.string   "oauth_token",  limit: 255
+    t.string   "oauth_secret", limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
 end
