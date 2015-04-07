140d52881058630ff06343d3228933d2e370a869
BerylSystems <vinay@berylsystems.com>
2015-04-07 18:50:00 +0530

    Integration of Images data in result -Now the result of Hashtag search include the tweet as well as images if user share any image -Same Images as well as tweet result is done for location -And also the result of a hashtag of a particular location is also showing tweet as well as images Also the error of code break is also resolved

--- twitter/app/controllers/home_controller.rb
+++ twitter/app/controllers/home_controller.rb
@@ -1,21 +1,18 @@
 class HomeController < ApplicationController
   def index
     get_client
-    
-  unless params[:address].blank?
-    lat = Geocoder.search(params[:address]).first.coordinates.first
-    lng = Geocoder.search(params[:address]).first.coordinates.last
+    unless params[:address].blank?
+      lat = Geocoder.search(params[:address]).first.coordinates.first
+      lng = Geocoder.search(params[:address]).first.coordinates.last
    
-	  unless params[:q].blank?
-      @tweet = @client.search(params[:q], :result_type => "recent", :geocode => "#{lat},#{lng},100mi").take(10)
+      @tweet = @client.search(params[:q], :include_entities=>true, :result_type => "recent", :geocode => "#{lat},#{lng},10mi").take(20)
     else
-  	  @tweet = @client.search("facebook", :result_type => "recent", :geocode => "#{lat},#{lng},10mi").take(5)
+      if params[:q].present?
+        @tweet = @client.search(params[:q], :include_entities=>true, :result_type => "recent").take(20)
+      else
+        @tweet = @client.search("facebook", :include_entities=>true, :result_type => "recent").take(20)
+      end
     end
-  else
-    @tweet = @client.search(params[:q], :result_type => "recent").take(10)
-  end
-
-
   end
 
   def get_client
@@ -24,6 +21,6 @@ class HomeController < ApplicationController
       config.consumer_secret     = "2HqQ1zcQDsCSauD5mKa8DDh6F8n6KXEH2vtw4m9MRBcs6QxBkB"
   	  config.access_token        = "2381587561-PAfyvBC02Ne8E6x4YFrla4LIQUOuOb7An1qzpLM"
   	  config.access_token_secret = "XrKEVZ6wZ9artQmn9GKYDiyZirB0yJf8GFJpqckzXdtCH"
-	end
+	  end
   end
 end
